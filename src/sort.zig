c641aa0dff23fcfc4eceb22773fc72cf0157271a
Alan Randon <alan.randon@outlook.com>
2023-09-26 16:40:19 +0100

    Add merge sort

--- src/sort.zig
+++ src/sort.zig
@@ -1,31 +1,82 @@
-// const std = @import("std");
-// const Allocator = std.mem.Allocator;
+const std = @import("std");
+const Allocator = std.mem.Allocator;
 
-// pub fn merge_sort(comptime T: type, data: []T) void {
-//     _ = data;
-// }
+pub fn mergeItem(comptime T: type) type {
+    const data_type = @typeInfo(T);
+    const item_type = data_type.Pointer.child;
+    return item_type;
+}
 
-// pub fn merge(comptime T: type, a: []T, b: []T, allocator: Allocator) []T {
-//     var a_index = 0;
-//     var b_index = 0;
-//     var index = 0;
-//     while (true) {
-//         switch (std.math.order(a[a_index], b[b_index])) {
-//             .gt => {
-//                 output[index] = a[a_index];
-//                 a_index += 1;
-//             },
-//             .lt | .eq => {
-//                 output[index] = b[b_index];
-//                 b_index += 1;
-//             },
-//         }
-//         index += 1;
-//     }
-// }
+/// Sort `data` and copy it to `buffer`
+fn mergeSort(data: anytype, buffer: anytype) void {
+    const item_type = mergeItem(@TypeOf(data));
+    const midpoint = data.len / 2;
+    if (midpoint == 0) {
+        for (data, 0..) |item, index| {
+            buffer[index] = item;
+        }
+        return;
+    }
 
-// test "merge sort works" {
-//     var data = []u8{ 4, 7, 1, 5 };
-//     merge_sort(data);
-//     try std.testing.expectEqual(data, []u8{ 1, 4, 5, 7 });
-// }
+    mergeSort(data[0..midpoint], buffer[0..midpoint]);
+    mergeSort(data[midpoint..data.len], buffer[midpoint..data.len]);
+
+    merge(item_type, buffer[0..midpoint], buffer[midpoint..data.len], data);
+    for (data, 0..) |item, index| {
+        buffer[index] = item;
+    }
+}
+
+pub fn merge(comptime T: type, a: []T, b: []T, merged: []T) void {
+    var a_index: usize = 0;
+    var b_index: usize = 0;
+    var index: usize = 0;
+
+    while (a_index < a.len and b_index < b.len) {
+        switch (std.math.order(a[a_index], b[b_index])) {
+            .lt, .eq => {
+                merged[index] = a[a_index];
+                a_index += 1;
+            },
+            .gt => {
+                merged[index] = b[b_index];
+                b_index += 1;
+            },
+        }
+        index += 1;
+    }
+
+    while (a_index < a.len) {
+        merged[index] = a[a_index];
+        index += 1;
+        a_index += 1;
+    }
+
+    while (b_index < b.len) {
+        merged[index] = b[b_index];
+        index += 1;
+        b_index += 1;
+    }
+}
+
+test "merge sort works" {
+    var data = [_]u8{ 4, 7, 1, 5 };
+    var sorted: [4]u8 = undefined;
+    mergeSort(@as([]u8, &data), undefined);
+    var expected = [_]u8{ 1, 4, 5, 7 };
+    try std.testing.expectEqualDeep(@as([]u8, &expected), @as([]u8, &sorted));
+}
+
+test "merge works" {
+    var a = [_]u8{ 1, 3, 4 };
+    var b = [_]u8{ 2, 5, 7 };
+    var merged: [6]u8 = undefined;
+    merge(
+        u8,
+        @as([]u8, &a),
+        @as([]u8, &b),
+        @as([]u8, &merged),
+    );
+    var expected = [_]u8{ 1, 2, 3, 4, 5, 7 };
+    try std.testing.expectEqualDeep(merged, expected);
+}
